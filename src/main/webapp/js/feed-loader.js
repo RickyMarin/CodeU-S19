a41525eb5e73b6c8f481d310396b3c287c41edbb
Caitlin Sanders <cmsander@syr.edu>
2019-03-14 04:31:42 -0400

    Adding Page links to top of User Feed and Message Feed

--- src/main/webapp/js/feed-loader.js
+++ src/main/webapp/js/feed-loader.js
@@ -61,8 +61,59 @@
 
   return messageDiv;
  }
+ function addLoginOrLogoutLinkToNavigation() {
+   const navigationElement = document.getElementById('navigation');
+   if (!navigationElement) {
+     console.warn('Navigation element not found!');
+     return;
+   }
+
+   fetch('/login-status')
+       .then((response) => {
+         return response.json();
+       })
+       .then((loginStatus) => {
+         if (loginStatus.isLoggedIn) {
+           navigationElement.appendChild(createListItem(createLink(
+               '/user-page.html?user=' + loginStatus.username, 'Your Page')));
+
+           navigationElement.appendChild(
+             createListItem(createLink('/feed.html', 'Message Feed')));
+           navigationElement.appendChild(
+               createListItem(createLink('/logout', 'Logout')));
+         } else {
+           navigationElement.appendChild(
+               createListItem(createLink('/login', 'Login')));
+         }
+       });
+ }
+
+ /**
+  * Creates an li element.
+  * @param {Element} childElement
+  * @return {Element} li element
+  */
+ function createListItem(childElement) {
+   const listItemElement = document.createElement('li');
+   listItemElement.appendChild(childElement);
+   return listItemElement;
+ }
+
+ /**
+  * Creates an anchor element.
+  * @param {string} url
+  * @param {string} text
+  * @return {Element} Anchor element
+  */
+ function createLink(url, text) {
+   const linkElement = document.createElement('a');
+   linkElement.appendChild(document.createTextNode(text));
+   linkElement.href = url;
+   return linkElement;
+ }
 
  // Fetch data and populate the UI of the page.
  function buildUI(){
+  addLoginOrLogoutLinkToNavigation();
   fetchMessages();
  }
