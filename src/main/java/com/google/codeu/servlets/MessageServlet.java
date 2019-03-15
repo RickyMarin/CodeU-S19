aa1d267d5794a8fc8b68be22f6b5c5794d539140
hedayaw <41341258+hedayaw@users.noreply.github.com>
2019-03-15 16:40:03 -0400

    Images Part 1 Test
    basic test to see if this code works so far

--- src/main/java/com/google/codeu/servlets/MessageServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageServlet.java
@@ -76,9 +76,18 @@ public class MessageServlet extends HttpServlet {
     }
 
     String user = userService.getCurrentUser().getEmail();
-    String text = Jsoup.clean(request.getParameter("text"), Whitelist.none());
+//    String text = Jsoup.clean(request.getParameter("text"), Whitelist.none());
+//
+//    Message message = new Message(user, text);
+//    datastore.storeMessage(message);
 
-    Message message = new Message(user, text);
+    String userText = Jsoup.clean(request.getParameter("text"),Whitelist.none());
+
+    String regex = "(https?://\\S+\\.(png|jpg))";
+    String replacement = "<img src=\"$1\"/>";
+    String textWithImagesReplaced = userText.replaceAll(regex, replacement);
+
+    Message message = new Message(user, textWithImagesReplaced);
     datastore.storeMessage(message);
 
     response.sendRedirect("/user-page.html?user=" + user);
---
+++ src/main/java/com/google/codeu/servlets/RegexExample.java
@@ -0,0 +1,25 @@
+package com.google.codeu.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.codeu.data.Datastore;
+import com.google.gson.JsonObject;
+
+//stats page will currently only display total number of messages
+
+public class RegexExample{
+    public static void main(String[] args){
+        String regex = "(https?://\\S+\\.(png|jpg))";
+        String replacement = "<img src=\"$1\" />";
+
+        String text = "here is an image: https://example.com/images/cat.jpg";
+
+        String result = text.replaceAll(regex, replacement);
+        System.out.println(result);
+    }
+}
