cfd82daf7ef7b9fa2dac8471eb2e342946546c1b
Caitlin Sanders <cmsander@syr.edu>
2019-03-15 17:08:40 -0700

    Adding new servlet and Datastore

---
+++ src/main/java/com/google/codeu/servlets/ChartServlet.java
@@ -0,0 +1,45 @@
+package com.google.codeu.servlets;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.codeu.data.Datastore;
+import com.google.codeu.data.Message;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+import com.google.cloud.language.v1.Document;
+import com.google.cloud.language.v1.Document.Type;
+import com.google.cloud.language.v1.LanguageServiceClient;
+import com.google.cloud.language.v1.Sentiment;
+
+/** Handles fetching and saving {@link Message} instances. */
+@WebServlet("/messagechart")
+public class ChartServlet extends HttpServlet {
+
+  private Datastore datastore;
+
+  @Override
+  public void init() {
+    datastore = new Datastore();
+  }
+
+  /**
+   * Responds with a JSON representation of {@link Message} data for a specific user. Responds with
+   * an empty array if the user is not provided.
+   */
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("application/json");
+    List<Message> msgList = datastore.getAllMessages();
+    Gson gson = new Gson();
+    String json = gson.toJson(msgList);
+    response.getWriter().println(json);
+}
+
+}
