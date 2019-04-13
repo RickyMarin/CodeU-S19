70b0791a44b2b1cd6dc9760e878fdbd1935a2e28
Ricky <ram8ny@virginia.edu>
2019-04-13 15:01:15 -0400

    maps to user Page initial commit

--- src/main/webapp/js/user-page-loader.js
+++ src/main/webapp/js/user-page-loader.js
@@ -154,6 +154,44 @@ function createLink(url, text) {
   linkElement.href = url;
   return linkElement;
 }
+MapsArray = new JsonArray();
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: {lat: 38.046252, lng: -78.506744}
+  });
+  var geocoder = new google.maps.Geocoder();
+
+  document.getElementById('submit').addEventListener('click', function() {
+    geocodeAddress(geocoder, map);
+  });
+}
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('address').value;
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === 'OK') {
+      resultsMap.setCenter(results[0].geometry.location);
+      // MapsArray.add(gson.toJsonTree(new Restaurant(results[0].geometry.lat, results[0].geometry.lang)));
+      var infowindow = new google.maps.InfoWindow({
+        content: "This is my favorite restaurant!"
+      });
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+
+      });
+
+      marker.addListener('click', function() {
+        infowindow.open(resultsMap, marker);
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+
+  });
+
+
+}
 
 /** Fetches data and populates the UI of the page. */
 function buildUI() {
@@ -162,5 +200,5 @@ function buildUI() {
   fetchAboutMe();
   showMessageFormIfViewingSelf();
   fetchMessages();
-
+  initMap();
 }
